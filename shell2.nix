8da77ad34b7576a8ff2e04f67113293ad4c20bb8
Christian Henry <christian.henry92@gmail.com>
2019-04-18 15:39:16 -0400

    add shell2.nix

---
+++ shell2.nix
@@ -0,0 +1,14 @@
+let 
+  pinnedPkgs = import ./pkgs-from-json.nix { json = ./nixos-18-09.json; };
+  myPackages = (import ./release6.nix { withHoogle = true; } );
+
+  projectDrvEnv = myPackages.project1.env.overrideAttrs (oldAttrs: rec {
+    buildInputs = oldAttrs.buildInputs ++ [ 
+      pinnedPkgs.haskellPackages.cabal-install
+      ];
+    shellHook = ''
+      export USERNAME="christian.henry"
+    '';
+  });
+in 
+  projectDrvEnv
